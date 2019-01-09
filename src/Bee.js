13919b63dcc8db9519ba0da76fbbc31aebd83ca9
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-09 10:41:15 -0800

    "Created all classes.  Was quick enough I forgot to commit between each"

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+	Grub.call(this);
+	this.color = 'yellow';
+	this.age = 5;
+	this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype); //this sets inheritance
+Bee.prototype.constructor = Bee;
